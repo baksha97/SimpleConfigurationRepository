9c6469566e2e0278c1380902aaba8b3485edbb14
Alfayed <baksha97@gmail.com>
2023-05-22 12:29:42 -0400

    cleanup + add demo

--- SimpleConfigurationDemo/SimpleConfigurationDemo/ContentView.swift
+++ SimpleConfigurationDemo/SimpleConfigurationDemo/ContentView.swift
@@ -1,21 +1,13 @@
-//
-//  ContentView.swift
-//  SimpleConfigurationDemo
-//
-//  Created by Travis Baksh on 5/22/23.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
   var body: some View {
-    EmptyView()
+    SimpleRepositoryView()
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
   }
 }
--- SimpleConfigurationDemo/SimpleConfigurationDemo/SimpleRepositoryView.swift
+++ SimpleConfigurationDemo/SimpleConfigurationDemo/SimpleRepositoryView.swift
@@ -1,12 +1,6 @@
-//
-//  SimpleRepositoryView.swift
-//  SimpleConfigurationDemo
-//
-//  Created by Travis Baksh on 5/22/23.
-//
-
 import SwiftUI
 import SimpleConfigurationRepository
+
 
 struct MySimpleConfig: SimpleConfigurationRepository.Model {
   var id: Int { version }
@@ -17,32 +11,80 @@ struct MySimpleConfig: SimpleConfigurationRepository.Model {
 }
 
 struct SimpleRepositoryView: View {
+  @StateObject
+  var viewModel = SimpleRepostioryViewModel()
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack {
+      result
+      Divider()
+      actions
+    }.onAppear(perform: viewModel.fetch)
   }
+  
+  @ViewBuilder
+  var result: some View {
+    if let configurationResult = viewModel.state.result {
+      Text(String(describing: configurationResult))
+    }
+  }
+  
+  var actions: some View {
+    Button("Perform Update") {
+      viewModel.update()
+    }
+  }
+}
+
+@MainActor
+class SimpleRepostioryViewModel: ObservableObject {
+  private let bucket: URL =
+    .init(
+      string: "https://raw.githubusercontent.com/baksha97/SimpleConfigurationRepository/main/config.json"
+    )!
+  
+  private let fallback: MySimpleConfig =
+    .init(
+      version: 1,
+      value: "Fallback"
+    )
+  
+  @Published var state: State = .init()
+  
+  private let repository: any ConfigurationRepository<MySimpleConfig>
+  init() {
+    repository = SimpleConfigurationRepository.build(
+      settings: .init(
+        fallback: fallback,
+        mode: .fileManager,
+        location: bucket
+      )
+    )
+  }
+  
+  func fetch() {
+    state.result = repository.current
+  }
+  
+  func update() {
+    Task {
+      do {
+        state.result = try await repository.update()
+      } catch {
+        state.error = error
+      }
+    }
+  }
+  
+  struct State {
+    var result: SimpleConfigurationRepository.Result<MySimpleConfig>? = nil
+    var error: Error? = nil
+  }
+  
 }
 
 struct SimpleRepositoryView_Previews: PreviewProvider {
   static var previews: some View {
     SimpleRepositoryView()
-  }
-}
-
-
-@MainActor
-class SimpleRepostioryViewModel: ObservableObject {
-  @Published var configuration: MySimpleConfig
-  
-  private let repository: any ConfigurationRepository<MySimpleConfig>
-  init() {
-    let fallback = MySimpleConfig(version: 1, value: "Fallback")
-    configuration = fallback
-    repository = SimpleConfigurationRepository.build(
-      settings: .init(
-        fallback: fallback,
-        mode: .fileManager,
-        location: .init(string: "")!
-      )
-    )
   }
 }
